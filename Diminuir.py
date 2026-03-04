95f099cb8cdb7580d5a883121598050215eb0b2f
vinithesant <gestao.vinicius.framefusion@gmail.com>
2026-03-03 22:14:46 -0300

    Add files via upload

---
+++ Diminuir.py
@@ -0,0 +1,19 @@
+# Exercício DIMINUIDOR
+# março 2026
+# Vinicius Sant
+
+print(" ####################################################################### ")
+print("                           VINICIUS SANT                                 ")
+print(" ####################################################################### ")
+
+a = input("Digite o 1º numero: ")
+a = int(a)
+
+b = input("Digite o 2ª numero: ")
+b = int(b)
+
+c = a - b
+
+print("A resposta é: ", c)
+
+input()
---
+++ Raiz.py
@@ -0,0 +1,16 @@
+# Exercício DIMINUIDOR
+# março 2026
+# Vinicius Sant
+
+print(" ####################################################################### ")
+print("                           VINICIUS SANT                                 ")
+print(" ####################################################################### ")
+
+a = input("Digite o 1º numero: ")
+a = int(a)
+
+c = a**(1/2)
+
+print("A resposta é: ", c)
+
+input()
